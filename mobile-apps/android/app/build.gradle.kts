be38f32ba785ed74c0cedaaf80fe20b51da9f322
unknown <yogikuat19@gmail.com>
2026-05-11 03:25:21 +0700

    Configure Firebase Android SDK and Google Services

--- mobile-apps/android/app/build.gradle.kts
+++ mobile-apps/android/app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -41,4 +41,11 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
+    // Add the dependency for the Firebase SDK for Google Analytics
+    implementation("com.google.firebase:firebase-analytics")
 }
